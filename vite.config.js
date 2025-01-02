b55ab7bf38843f973f3c6d13af0aa2e4f4fe891f
Hay <hay82036@gmail.com>
2025-01-02 06:35:29 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  base: './'
 })
